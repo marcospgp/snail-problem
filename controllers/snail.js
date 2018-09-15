dfd365dd2385f800679d64736fac463adb2043fa
marcospgp <marcospereiradev@gmail.com>
2018-09-15 17:10:25 +0100

    Add POST /snail endpoint logic

--- controllers/snail.js
+++ controllers/snail.js
@@ -1,4 +1,14 @@
 /**
+ * Calculates Snail well-climbing success or failure
+ */
+function climbWell(height, up, down, fatigue) {
+  return {
+    success: true,
+    day: 4
+  };
+}
+
+/**
  * GET /snail/
  * Snail form.
  */
@@ -13,7 +23,41 @@ exports.getSnail = (req, res) => {
  * Snail form.
  */
 exports.postSnail = (req, res) => {
-  res.render('snail.ejs', {
-    title: 'Snail'
+  if (
+    !req.body
+    || !req.body.h
+    || !req.body.u
+    || !req.body.d
+    || !req.body.f
+  ) {
+    res.send(400, { status: '400 BAD REQUEST', message: 'missing parameter' });
+    return;
+  }
+
+  const { h, u, d, f } = req.body; // eslint-disable-line
+
+  const max = Math.max(h, u, d, f);
+  const min = Math.max(h, u, d, f);
+
+  if (max > 100 || min < 1) {
+    res.send(400, { status: '400 BAD REQUEST', message: 'parameters must be between 1 and 100 (inclusive)' });
+    return;
+  }
+
+  const { success, day } = climbWell(h, u, d, f);
+
+  let result = '';
+
+  if (success) {
+    result += 'success';
+  } else {
+    result += 'failure';
+  }
+
+  result += ` on day ${day}`;
+
+  res.send(200, {
+    status: '200 OK',
+    mesasge: result
   });
 };
